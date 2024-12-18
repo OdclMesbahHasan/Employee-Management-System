db6b57fc6bf2729870b6e3aabe8c826384394a53
OdclMesbahHasan <mdmesbahhasanodcl@gmail.com>
2024-12-18 17:47:40 +0600

    EmployeeServiceImpl implemented

--- src/main/java/com/mesbah/employyeMgt/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/mesbah/employyeMgt/service/impl/EmployeeServiceImpl.java
@@ -1,11 +1,20 @@
 package com.mesbah.employyeMgt.service.impl;
 
 import com.mesbah.employyeMgt.dto.EmployeeDto;
+import com.mesbah.employyeMgt.entity.Employee;
+import com.mesbah.employyeMgt.mapper.EmployeeMapper;
+import com.mesbah.employyeMgt.repository.EmployeeRepository;
 import com.mesbah.employyeMgt.service.EmployeeService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
+    private EmployeeRepository employeeRepository;
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
-        return null;
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+
     }
 }
